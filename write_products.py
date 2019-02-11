aadc937b41792577a75507bd96e962be205a2ad9
Caroline Feeney <carolinefeeney@Carolines-MacBook-Pro-5.local>
2019-02-11 15:51:24 -0500

    Changed from teams to groceries

---
+++ write_products.py
@@ -0,0 +1,14 @@
+# csv-mgmt/write_teams.py
+
+import csv
+
+csv_file_path = "teams.csv" # a relative filepath
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
