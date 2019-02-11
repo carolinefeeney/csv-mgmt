aedde64d230637214134b31266a223d88904ff54
Caroline Feeney <carolinefeeney@Carolines-MacBook-Pro-5.local>
2019-02-11 15:48:39 -0500

    created read_teams and write_teams

---
+++ read_teams.py
@@ -0,0 +1,11 @@
+# csv-mgmt/read_teams.py
+
+import csv
+
+csv_file_path = "teams.csv" # a relative filepath
+
+with open(csv_file_path, "r") as csv_file: # "r" means "open the file for reading"
+    reader = csv.DictReader(csv_file) # assuming your CSV has headers
+    # reader = csv.reader(csv_file) # if your CSV doesn't have headers
+    for row in reader:
+        print(row["city"], row["name"])
